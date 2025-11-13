b428abfe4f6d93d6ada57377d7b9974edca07ade
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-11-13 14:14:52 +0500

    feat: build fundament of the project

--- src/pages/dashboard/components/DashboardHeader.tsx
+++ src/pages/dashboard/components/DashboardHeader.tsx
@@ -9,59 +9,61 @@ import { Link, useLocation } from 'react-router-dom';
 import { DashboardContext } from '..';
 
 const DashboardHeader = ({ children, ...props }: FlexProps) => {
-  const location = useLocation();
-  const pathNames = useMemo(
-    () => location.pathname.split('/').filter(name => name),
-    [location]
-  );
-  const { navbarList } = useNavbarList();
-  const isMobileNavBottom = useSelector(
-    (store: RootState) => store.authSlice.isMobileNavBottom
-  );
-  const { setIsNavbarActive, toggleMenuButtonRef, isMobile } =
-    useContext(DashboardContext);
+    const location = useLocation();
+    const pathNames = useMemo(
+        () => location.pathname.split('/').filter(name => name),
+        [location]
+    );
+    const { navbarList } = useNavbarList();
+    const isMobileNavBottom = useSelector(
+        (store: RootState) => store.authSlice.isMobileNavBottom
+    );
+    const { setIsNavbarActive, toggleMenuButtonRef, isMobile } =
+        useContext(DashboardContext);
 
-  return (
-    <Flex justify="space-between" gap={18} align="center">
-      <Flex gap={4} vertical {...props}>
-        <Breadcrumb
-          style={{ marginBottom: 16 }}
-          items={[
-            ...pathNames.map((name, index) => {
-              const fullPath = `/${pathNames.slice(0, index + 1).join('/')}`;
-              const navItem = navbarList.find(nav => nav.path === fullPath);
-              return {
-                title: (
-                  <Link to={fullPath}>
-                    {toFirstCapitalLetter((navItem && navItem.title) || name)}
-                  </Link>
-                ),
-              };
-            }),
-          ]}
-        />
-        {children}
-      </Flex>
-      <Flex gap={12} align="center">
-        <Flex gap={12} align="center">
-          <Avatar shape="circle" size="large" style={{ background: '#1677ff' }}>
-            AB
-          </Avatar>
-          <Flex vertical gap={2}>
-            <Typography.Text strong>Admin Foydalanuvchi</Typography.Text>
-            <Typography.Text>Administrator</Typography.Text>
-          </Flex>
+    return (
+        <Flex justify="space-between" gap={18} align="center">
+            <Flex gap={4} vertical {...props}>
+                <Breadcrumb
+                    style={{ marginBottom: 16 }}
+                    items={[
+                        ...pathNames.map((name, index) => {
+                            const fullPath = `/${pathNames.slice(0, index + 1).join('/')}`;
+                            const navItem = navbarList.find(nav => nav.path === fullPath);
+                            return {
+                                title: (
+                                    <Link to={fullPath}>
+                                        {toFirstCapitalLetter((navItem && navItem.title) || name)}
+                                    </Link>
+                                ),
+                            };
+                        }),
+                    ]}
+                />
+                {children}
+            </Flex>
+            <Flex gap={12} align="center">
+                <Flex gap={12} align="center">
+                    <Avatar shape="circle" size="large" style={{ background: '#1677ff' }}>
+                        AB
+                    </Avatar>
+                    {!isMobile && (
+                        <Flex vertical gap={2}>
+                            <Typography.Text strong>Admin Foydalanuvchi</Typography.Text>
+                            <Typography.Text>Administrator</Typography.Text>
+                        </Flex>
+                    )}
+                </Flex>
+                {!isMobileNavBottom && isMobile && (
+                    <Button
+                        icon={<MenuOutlined />}
+                        onClick={() => setIsNavbarActive(prev => !prev)}
+                        ref={toggleMenuButtonRef}
+                    />
+                )}
+            </Flex>
         </Flex>
-        {!isMobileNavBottom && isMobile && (
-          <Button
-            icon={<MenuOutlined />}
-            onClick={() => setIsNavbarActive(prev => !prev)}
-            ref={toggleMenuButtonRef}
-          />
-        )}
-      </Flex>
-    </Flex>
-  );
+    );
 };
 
 export default DashboardHeader;
